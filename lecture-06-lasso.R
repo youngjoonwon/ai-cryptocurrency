9cc0b20b446d573f89eab0c76a5096eaaf178a52
Youngjoon Won <young@macbook-m2.local>
2024-05-27 13:34:12 +0900

    edited

--- lecture-06-lasso.R
+++ lecture-06-lasso.R
@@ -1,3 +1,18 @@
+####### How to install R using Homebrew:
+# $brew install r or $brew cask install rstudio
+# $sudo r
+
+####### How to install R packages:
+# $sudo r
+# $install.packages("glmnet")
+
+####### How to run R script:
+# $Rscript your-file.R
+
+####### Usage: Finding a Model
+# arguemnts: start-time end-time exchange coin-symbol mid5
+# $Rscript ./lasso-ai-cryto.R '2024-05-01T00:00:00' '2024-05-01T23:59:00' upbit BTC mid5
+
 library('stringr')
 library('glmnet')
 
@@ -15,18 +30,17 @@ args<-commandArgs(TRUE)
 #args[3] exchange
 #args[4] 
 
-filtered = paste(args[1],args[2],args[3],args[4],'filtered-10-2',args[5],sep="-")
-model_file = paste(args[2],args[3],args[4],args[5],'lasso-10s-2std',sep='-')
+filtered = paste(args[1],args[2],args[3],args[4],'filtered-5-2',args[5],sep="-")
+model_file = paste(args[2],args[3],args[4],args[5],'lasso-5s-2std',sep='-')
+
 #return_file
 filtered <- str_remove_all(filtered,":")
 model_file <- str_remove_all(model_file,":")
 
-filtered = paste ("~/tmp/", filtered, ".csv", sep="")
+filtered = paste ("./", filtered, ".csv", sep="")
 message(filtered)
 message(model_file)
-model_file = paste ("./model/", model_file, ".csv", sep="")
-
-#filtered_10s_2std = read.csv("2021-01-01T000000-2021-01-03T000000-bithumb-BTC-filtered-10-2-mid5.csv")
+model_file = paste ("./", model_file, ".csv", sep="")
 
 filtered = read.csv(filtered)
 mid_std = sd(filtered$mid_price)
@@ -36,21 +50,19 @@ message (round(mid_std,0))
 
 filtered_no_time_mid = subset(filtered, select=-c(mid_price,timestamp))
 
-#y = filtered_no_time_mid[,28]  # return
-#x = filtered_no_time_mid[,1:27] # indicators
-
 y = filtered_no_time_mid$return
 x = subset(filtered_no_time_mid, select=-c(return))
 
 #quit()
 
 x<-as.matrix(x)
+#model_ols<-lm(y~x)
+#model_lasso<-glmnet(x,y)
 
 #cv_fit <- cv.glmnet(x=x, y=y, alpha=0, intercept=FALSE, lower.limits=0, nfolds=10) #ridge
 cv_fit <- cv.glmnet(x=x, y=y, alpha=1, intercept=FALSE, lower.limits=0, nfolds=5) #lasso
 
 fit <- glmnet(x=x, y=y, alpha = 1, lambda=cv_fit$lambda.1se, intercept=FALSE, lower.limits=0,)
-#fit <- glmnet(x=x, y=y, alpha = 1, lambda=cv_fit$lambda.min, intercept=FALSE, lower.limits=0,)
 #coef(fit)
 
 df <- extract(fit, s=0.1)
